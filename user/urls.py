b144d8c11f862a9570a9a77273791e8a02eefcd7
DF_XYZ <dfxyz1@gmail.com>
2016-03-22 17:41:09 +0800

    cancel url configures merging

--- user/urls.py
+++ user/urls.py
@@ -4,9 +4,9 @@ from user import views
 
 urlpatterns = [
     url(r'^$', views.user_root, name='root'),
-    url(r'^total/', views.user_total, name='total'),
-    url(r'^token/', views.user_token, name='token'),
-    url(r'^username/', views.user_username, name='username'),
-    url(r'^password/', views.user_password, name='password'),
-    url(r'^id/', views.user_id, name='id'),
+    url(r'^total/$', views.user_total, name='total'),
+    url(r'^token/$', views.user_token, name='token'),
+    url(r'^username/$', views.user_username, name='username'),
+    url(r'^password/$', views.user_password, name='password'),
+    url(r'^id/$', views.user_id, name='id'),
 ]
