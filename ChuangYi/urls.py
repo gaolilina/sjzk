a213a175672d0d4ca8a7cf062b7a56d71c290210
DF_XYZ <dfxyz1@gmail.com>
2016-03-24 16:27:54 +0800

    move url conf back to ChuangYi.urls

--- ChuangYi/urls.py
+++ ChuangYi/urls.py
@@ -3,6 +3,16 @@
 
 from django.conf.urls import url, include
 
+import user.views
+
+user_urls = [
+    url(r'^$', user.views.user_root, name='root'),
+    url(r'^total/$', user.views.user_total, name='total'),
+    url(r'^token/$', user.views.user_token, name='token'),
+    url(r'^username/$', user.views.user_username, name='username'),
+    url(r'^password/$', user.views.user_password, name='password'),
+]
+
 urlpatterns = [
-    url(r'^user/', include('user.urls', namespace='user')),
+    url(r'^user/', include(user_urls, namespace='user')),
 ]
