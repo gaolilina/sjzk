3124d0c8cb5b494cdfdeef95fc20abe8b0eb1145
zq35399652 <15620953635@163.com>
2016-05-16 10:23:01 +0800

    create_team and get_team_list(2)

--- ChuangYi/settings.py
+++ ChuangYi/settings.py
@@ -66,13 +66,18 @@ WSGI_APPLICATION = 'ChuangYi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cy',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '123.206.24.226',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
+
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': 'cy',
+    #    'USER': 'postgres',
+    #    'PASSWORD': 'postgres',
+    #    'HOST': '123.206.24.226',
+    #    'PORT': '5432',
+    #},
 }
 
 # Internationalization
