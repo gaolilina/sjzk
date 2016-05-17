2fec8f3b40d94c2e499baaff1f4434baae260274
DF_XYZ <dfxyz1@gmail.com>
2016-05-17 11:26:50 +0800

    make django test server serve media path

--- main/urls/__init__.py
+++ main/urls/__init__.py
@@ -1,10 +1,12 @@
 # ChuangYi URL Configuration
 # https://docs.djangoproject.com/en/1.9/topics/http/urls/
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 
+from ChuangYi import settings
 from . import self, users
 
 urlpatterns = [
     url(r'^self/', include(self.urls, namespace='self')),
     url(r'^users/', include(users.urls, namespace='user')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
