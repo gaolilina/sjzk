e5366f54d55807953ac35d49b4518874a0678f17
DF_XYZ <dfxyz1@gmail.com>
2016-05-25 12:47:05 +0800

    refactor message-related models

--- main/models/user/message.py
+++ main/models/user/message.py
@@ -1,88 +1,21 @@
 from datetime import datetime
 
 from django.db import models
-from django.db.models import Q
-
-
-class MessageManager(models.Manager):
-    def get_queryset(self):
-        return super(MessageManager, self).get_queryset().filter(
-            sender__is_enabled=True, receiver__is_enabled=True,
-        )
-
-    def related_to(self, user, other_user=None):
-        """
-        获取与某个或某两个用户相关的消息的 Query Set
-
-        """
-        if other_user:
-            return self.get_queryset().filter(
-                Q(sender=user, receiver=other_user) |
-                Q(sender=other_user, receiver=user)
-            )
-        else:
-            return self.get_queryset().filter(
-                Q(sender=user) | Q(receiver=user)
-            )
-
-
-class Message(models.Model):
-    """
-    消息
-
-    """
-    sender = models.ForeignKey('User', models.CASCADE, '+')
-    receiver = models.ForeignKey('User', models.CASCADE, '+')
-    data = models.TextField(max_length=256, db_index=True)
-    is_read = models.BooleanField(default=False, db_index=True)
-    create_time = models.DateTimeField(default=datetime.now, db_index=True)
-
-    enabled = MessageManager()
-
-    class Meta:
-        db_table = 'message'
-        ordering = ['-create_time']
 
 
 class ContactManager(models.Manager):
     def get_queryset(self):
         return super(ContactManager, self).get_queryset().filter(
-            user_a__is_enabled=True, user_b__is_enabled=True,
-        )
-
-    def related_to(self, user):
-        """
-        获取与某个用户相关的联系人的 Query Set
-
-        """
-        return self.get_queryset().filter(Q(user_a=user) | Q(user_b=user))
-
-    def update_record(self, user, other_user, last_message):
-        """
-        更新联系人记录
-
-        :param user: 用户
-        :param other_user: 另一用户
-        :param last_message: 最近一条消息
-
-        """
-        qs = self.get_queryset().filter(Q(user_a=user, user_b=other_user) |
-                                        Q(user_b=user, user_a=other_user))
-        if qs.exists():
-            qs[0].last_message = last_message
-            qs[0].save()
-        else:
-            r = self.create(user_a=user, user_b=other_user,
-                            last_message=last_message)
+            user__is_enabled=True, contact__is_enabled=True)
 
 
 class Contact(models.Model):
     """
-    联系人记录
+    联系记录
 
     """
-    user_a = models.ForeignKey('User', models.CASCADE, '+')
-    user_b = models.ForeignKey('User', models.CASCADE, '+')
+    user = models.ForeignKey('User', models.CASCADE, 'contacts')
+    contact = models.ForeignKey('User', models.CASCADE, '+')
     last_message = models.ForeignKey('Message', models.CASCADE, '+')
     update_time = models.DateTimeField(auto_now=True, db_index=True)
 
@@ -91,3 +24,32 @@ class Contact(models.Model):
     class Meta:
         db_table = 'contact'
         ordering = ['-update_time']
+
+
+class MessageManager(models.Manager):
+    def get_queryset(self):
+        return super(MessageManager, self).get_queryset().filter(
+            user__is_enabled=True, other_user__is_enabled=True)
+
+
+class Message(models.Model):
+    """
+    消息收发凭据
+
+    """
+    user = models.ForeignKey('User', models.CASCADE, 'messages')
+    other_user = models.ForeignKey('User', models.CASCADE, '+')
+    direction = models.IntegerField(choices=(('收', 0), ('发', 1)))
+    is_read = models.BooleanField(default=False, db_index=True)
+    create_time = models.DateTimeField(default=datetime.now, db_index=True)
+
+    content = models.TextField(default='', max_length=256, db_index=True)
+    is_sharing = models.BooleanField(default=False, db_index=True)
+    sharing_type = models.TextField(default=None, null=True)
+    sharing_object_id = models.IntegerField(default=None, null=True)
+
+    enabled = MessageManager()
+
+    class Meta:
+        db_table = 'message_receipt'
+        ordering = ['-create_time']
--- main/views/user/message.py
+++ main/views/user/message.py
@@ -1,12 +1,10 @@
-import json
-
 from django import forms
 from django.db import transaction
 from django.http import JsonResponse
 from django.views.generic import View
 
 from main.decorators import require_token, validate_input, check_object_id
-from main.models import User, Contact, Message
+from main.models import User
 from main.responses import Http200
 
 
@@ -30,36 +28,29 @@ class Contacts(View):
                 id: 用户ID
                 username: 用户名
                 icon_url: 头像URL
-                count: 总消息数
                 unread_count: 未读消息数
+                update_time: 最近联系时间
                 last_message: 最近一条消息
-                    id: 消息ID
-                    sender_id: 发送者ID
-                    receiver_id: 接受者ID
-                    data: 消息数据（JSON）
-                    create_time: 发送时间
+                    content: 消息内容
+                    is_sharing: 是否分享消息
+                    sharing_type: 分享类型
+                    sharing_object_id: 分享对象ID
         """
         i, j = offset, offset + limit
-        qs = Contact.enabled.related_to(request.user)
-        c = qs.count()
-        l = []
-        for r in qs[i:j]:
-            u = r.user_a if request.user != r.user_a else r.user_b
-            l.append({
-                'id': u.id,
-                'username': u.username,
-                'icon_url': u.icon_url,
-                'count': Message.enabled.related_to(request.user, u).count(),
-                'unread_count': Message.enabled.filter(
-                    sender=u, receiver=request.user, is_read=False).count(),
-                'last_message': {
-                    'id': r.last_message.id,
-                    'sender_id': r.last_message.sender.id,
-                    'receiver_id': r.last_message.receiver.id,
-                    'data': r.last_message.data,
-                    'create_time': r.last_message.create_time,
-                }
-            })
+        c = request.user.contacts.count()
+        qs = request.user.contacts[i:j]
+        l = [{'id': r.contact.id,
+              'username': r.contact.username,
+              'icon_url': r.contact.icon_url,
+              'unread_count': request.user.messages.filter(
+                  other_user=r.contact, direction=0, is_read=False).count(),
+              'update_time': r.update_time,
+              'last_message': {
+                  'content': r.last_message.content,
+                  'is_sharing': r.last_message.is_sharing,
+                  'sharing_type': r.last_message.sharing_type,
+                  'sharing_object_id': r.last_message.sharing_object_id,
+              }} for r in qs]
         return JsonResponse({'count': c, 'list': l})
 
 
@@ -82,28 +73,30 @@ class Messages(View):
             count: 消息总数
             unread_count: （拉取之后）未读消息数
             list: 消息列表
-                id: 消息ID
-                sender_id: 发送者ID
-                receiver_id: 接受者ID
-                data: 消息数据（JSON）
+                direction: 收发方向，0-收，1-发
+                content: 消息内容
+                is_sharing: 是否分享消息
+                sharing_type: 分享类型
+                sharing_object_id: 分享对象ID
                 create_time: 消息发送时间
         """
         i, j = offset, offset + limit
-        qs = Message.enabled.related_to(request.user, user)
+        qs = request.user.messages.filter(other_user=user)
         c = qs.count()
-        l = [{'id': r.id,
-              'sender_id': r.sender.id,
-              'receiver_id': r.receiver.id,
-              'data': r.data,
+        l = [{'direction': r.direction,
+              'content': r.content,
+              'is_sharing': r.is_sharing,
+              'sharing_type': r.sharing_type,
+              'sharing_object_id': r.sharing_object_id,
               'create_time': r.create_time} for r in qs[i:j]]
         # 将拉取的消息标记为已读
         ids = qs[i:j].values('id')
-        Message.enabled.filter(id__in=ids).update(is_read=True)
-        uc = Message.enabled.filter(
-            sender=user, receiver=request.user, is_read=False).count()
+        request.user.messages.filter(id__in=ids).update(is_read=True)
+        uc = request.user.messages.filter(
+            other_user=user, is_read=False).count()
         return JsonResponse({'count': c, 'unread_count': uc, 'list': l})
 
-    post_dict = {'content': forms.CharField(max_length=200)}
+    post_dict = {'content': forms.CharField(max_length=256)}
 
     @check_object_id(User.enabled, 'user')
     @require_token
@@ -113,11 +106,20 @@ class Messages(View):
         向某用户发送消息
 
         """
-        data = {'content': content, 'is_sharing': False}
-        data = json.dumps(data)
-        # 保存消息数据
-        msg = Message.enabled.create(
-            sender=request.user, receiver=user, data=data)
-        # 更新联系人消息
-        Contact.enabled.update_record(request.user, user, msg)
+        msg = request.user.messages.create(other_user=request.user, direction=1,
+                                           content=content, is_sharing=False)
+        r, created = request.user.contacts.get_or_create(
+            contact=user, defaults={'last_message': msg})
+        if not created:
+            r.last_message = msg
+            r.save()
+
+        msg = user.messages.create(other_user=request.user, direction=0,
+                                   content=content, is_sharing=False)
+        r, created = user.contacts.get_or_create(
+            contact=request.user, defaults={'last_message': msg})
+        if not created:
+            r.last_message = msg
+            r.save()
+
         return Http200()
