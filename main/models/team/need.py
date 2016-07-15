fde3f9b9ffe5f9d48a88eab15a67650bc77b8fd3
zq_dell <15620953635@163.com>
2016-07-15 17:29:01 +0800

    teem_need temp

--- main/models/team/need.py
+++ main/models/team/need.py
@@ -1,6 +1,49 @@
 from datetime import datetime
 
 from django.db import models
+from main.models.user.experience import WorkExperience
+
+
+class NeedEducationExperience(WorkExperience):
+    """
+    教育经历需求
+    """
+    need = models.ForeignKey('TeemNeed', models.CASCADE, 'education_experience')
+
+    class Meta:
+        db_table = 'need_education_experience'
+
+
+class NeedWorkExperience(WorkExperience):
+    """
+    工作经历需求
+    """
+    need = models.ForeignKey('TeemNeed', models.CASCADE, 'work_experience')
+
+    class Meta:
+        db_table = 'need_work_experience'
+
+
+class NeedFieldworkExperience(WorkExperience):
+    """
+    实习经历需求
+
+    """
+    need = models.ForeignKey('TeemNeed', models.CASCADE,
+                             'fieldwork_experiences')
+
+    class Meta:
+        db_table = 'need_fieldwork_experience'
+
+
+class NeedProjectExperience(WorkExperience):
+    """
+    需求项目经历
+    """
+    need = models.ForeignKey('TeemNeed', models.CASCADE, 'project_experience')
+
+    class Meta:
+        db_table = 'need_project_experience'
 
 
 class TeamNeedManager(models.Manager):
@@ -16,14 +59,16 @@ class TeamNeed(models.Model):
     """
     team = models.ForeignKey('Team', models.CASCADE, 'needs')
 
-    description = models.TextField(
-            '需求描述', max_length=100, db_index=True)
+    title = models.TextField(
+            '需求标题', max_length=20, db_index=True)
     status = models.IntegerField(
             '状态', default=0, choices=(('未满足', 0), ('已满足', 1)))
     number = models.IntegerField(
             '需求人数', default=-1, db_index=True)
-    gender = models.IntegerField(
-            '性别要求', default=0, choices=(('不限', 0), ('男', 1), ('女', 2)))
+    field = models.CharField(
+            '领域', max_length=10, db_index=True)
+    skills = models.CharField(
+            '技能', max_length=10, db_index=True)
 
     create_time = models.DateTimeField(
             '创建时间', default=datetime.now, db_index=True)
@@ -35,5 +80,59 @@ class TeamNeed(models.Model):
     enabled = TeamNeedManager()
 
     class Meta:
-        db_table = 'team_need'
         ordering = ['-create_time']
+
+
+class MemberNeed(TeamNeed):
+    """
+    人员需求
+    """
+    age = models.CharField(
+            '年龄区间', default='不限')
+    gender = models.IntegerField(
+            '性别要求', default=0, choices=(('不限', 0), ('男', 1), ('女', 2)))
+    qualification = models.CharField()
+    graduate_time = models.TimeField()
+    profession = models.CharField()
+    work_experience = models.CharField()
+    practice_experience = models.CharField()
+    project_experience = models.CharField()
+
+    class Meta:
+        db_table = 'member_need'
+
+
+class OutsourceNeed(TeamNeed):
+    """
+    外包需求
+    """
+    age = models.CharField(
+            '年龄区间', default='不限')
+    gender = models.IntegerField(
+            '性别要求', default=0, choices=(('不限', 0), ('男', 1), ('女', 2)))
+    qualification = models.CharField()
+    profession = models.CharField()
+    expend = models.IntegerField('费用')
+    expend_unit = models.IntegerField(
+            '费用单位', choices=(('项', 0), ('天', 1), ('人', 2)))
+    description = models.TextField(
+            '需求描述', max_length=100, db_index=True)
+    task_time = models.TimeField('任务时间区间')
+
+    class Meta:
+        db_table = 'outsource_need'
+
+
+class UndertakeNeed(TeamNeed):
+    """
+    承接需求
+    """
+    expend = models.IntegerField('费用')
+    expend_unit = models.IntegerField(
+            '费用单位', choices=(('项', 0), ('天', 1), ('人', 2)))
+    description = models.TextField(
+            '需求描述', max_length=100, db_index=True)
+    undertake_time = models.TimeField('承接时间区间')
+
+    class Meta:
+        db_table = 'undertake_need'
