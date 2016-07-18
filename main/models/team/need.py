a682d69ab7749ac5237ce99410ba54e65fe9fc0a
zq35399652 <15620953635@163.com>
2016-07-18 22:23:36 +0800

    need interface temp1

--- main/models/team/need.py
+++ main/models/team/need.py
@@ -1,49 +1,6 @@
 from datetime import datetime
 
 from django.db import models
-from main.models.user.experience import WorkExperience
-
-
-class NeedEducationExperience(WorkExperience):
-    """
-    教育经历需求
-    """
-    need = models.ForeignKey('TeemNeed', models.CASCADE, 'education_experience')
-
-    class Meta:
-        db_table = 'need_education_experience'
-
-
-class NeedWorkExperience(WorkExperience):
-    """
-    工作经历需求
-    """
-    need = models.ForeignKey('TeemNeed', models.CASCADE, 'work_experience')
-
-    class Meta:
-        db_table = 'need_work_experience'
-
-
-class NeedFieldworkExperience(WorkExperience):
-    """
-    实习经历需求
-
-    """
-    need = models.ForeignKey('TeemNeed', models.CASCADE,
-                             'fieldwork_experiences')
-
-    class Meta:
-        db_table = 'need_fieldwork_experience'
-
-
-class NeedProjectExperience(WorkExperience):
-    """
-    需求项目经历
-    """
-    need = models.ForeignKey('TeemNeed', models.CASCADE, 'project_experience')
-
-    class Meta:
-        db_table = 'need_project_experience'
 
 
 class TeamNeedManager(models.Manager):
@@ -59,10 +16,12 @@ class TeamNeed(models.Model):
     """
     team = models.ForeignKey('Team', models.CASCADE, 'needs')
 
+    type = None
     title = models.TextField(
             '需求标题', max_length=20, db_index=True)
     status = models.IntegerField(
-            '状态', default=0, choices=(('未满足', 0), ('已满足', 1)))
+            '状态', default=0, choices=(
+            ('未满足', 0), ('已满足', 1), ('已删除', 2)))
     number = models.IntegerField(
             '需求人数', default=-1, db_index=True)
     field = models.CharField(
@@ -73,7 +32,7 @@ class TeamNeed(models.Model):
     create_time = models.DateTimeField(
             '创建时间', default=datetime.now, db_index=True)
     deadline = models.DateTimeField(
-        '截止时间', default=None, blank=True, null=True, db_index=True)
+            '截止时间', default=None, blank=True, null=True, db_index=True)
     update_time = models.DateTimeField(
             '更新时间', auto_now=True, db_index=True)
 
@@ -83,56 +42,85 @@ class TeamNeed(models.Model):
         ordering = ['-create_time']
 
 
-class MemberNeed(TeamNeed):
+class TeamMemberNeed(TeamNeed):
     """
     人员需求
     """
-    age = models.CharField(
-            '年龄区间', default='不限')
+    type = models.IntegerField(
+            '需求类型', default=1)
+    age_min = models.IntegerField(
+            '最小年龄', default='')
+    age_max = models.IntegerField(
+            '最大年龄', default='')
     gender = models.IntegerField(
             '性别要求', default=0, choices=(('不限', 0), ('男', 1), ('女', 2)))
-    qualification = models.CharField()
-    graduate_time = models.TimeField()
-    profession = models.CharField()
-    work_experience = models.CharField()
-    practice_experience = models.CharField()
-    project_experience = models.CharField()
+
+    degree = models.IntegerField(
+        '学历', default=0, choices=(
+            ('其他', 0), ('初中', 1), ('高中', 2), ('大专', 3),
+            ('本科', 4), ('硕士', 5), ('博士', 6)))
+    major = models.CharField(
+        '专业', max_length=20, default='', db_index=True)
+    graduate_time = models.DateField(
+        '毕业时间', default='', db_index=True)
+    work_experience = models.CharField(
+            '工作经历', max_length=100, default='', db_index=True)
+    practice_experience = models.CharField(
+            '实习经历', max_length=100, default='', db_index=True)
+    project_experience = models.CharField(
+            '项目经历', max_length=100, default='', db_index=True)
 
     class Meta:
         db_table = 'member_need'
 
 
-class OutsourceNeed(TeamNeed):
+class TeamOutsourceNeed(TeamNeed):
     """
     外包需求
     """
-    age = models.CharField(
-            '年龄区间', default='不限')
+    type = models.IntegerField(
+            '需求类型', default=2)
+    age_min = models.IntegerField(
+            '最小年龄', default='')
+    age_max = models.IntegerField(
+            '最大年龄', default='')
     gender = models.IntegerField(
             '性别要求', default=0, choices=(('不限', 0), ('男', 1), ('女', 2)))
-    qualification = models.CharField()
-    profession = models.CharField()
-    expend = models.IntegerField('费用')
+    degree = models.IntegerField(
+        '学历', default=0, choices=(
+            ('其他', 0), ('初中', 1), ('高中', 2), ('大专', 3),
+            ('本科', 4), ('硕士', 5), ('博士', 6)))
+    major = models.CharField(
+        '专业', max_length=20, default='', db_index=True)
+    expend = models.IntegerField('费用', default=-1)
     expend_unit = models.IntegerField(
-            '费用单位', choices=(('项', 0), ('天', 1), ('人', 2)))
+            '费用单位', default=0, choices=(('项', 0), ('天', 1), ('人', 2)))
     description = models.TextField(
-            '需求描述', max_length=100, db_index=True)
-    task_time = models.TimeField('任务时间区间')
+            '需求描述', max_length=100, default='', db_index=True)
+    start_time = models.DateTimeField(
+            '开始时间', default=datetime.now, db_index=True)
+    end_time = models.DateTimeField(
+            '结束时间', default='', db_index=True)
 
     class Meta:
         db_table = 'outsource_need'
 
 
-class UndertakeNeed(TeamNeed):
+class TeamUndertakeNeed(TeamNeed):
     """
     承接需求
     """
+    type = models.IntegerField(
+            '需求类型', default=3)
     expend = models.IntegerField('费用')
     expend_unit = models.IntegerField(
             '费用单位', choices=(('项', 0), ('天', 1), ('人', 2)))
     description = models.TextField(
             '需求描述', max_length=100, db_index=True)
-    undertake_time = models.TimeField('承接时间区间')
+    start_time = models.DateTimeField(
+            '开始时间', default=datetime.now, db_index=True)
+    end_time = models.DateTimeField(
+            '结束时间', default='', db_index=True)
 
     class Meta:
         db_table = 'undertake_need'
--- main/views/team/need.py
+++ main/views/team/need.py
@@ -5,13 +5,15 @@ from django.http import JsonResponse
 
 from main.decorators import require_token, check_object_id, \
     validate_input, validate_json_input
-from main.models.team.need import TeamNeed
+from main.models.team.need import TeamMemberNeed, \
+    TeamOutsourceNeed, TeamUndertakeNeed
 from main.models.location import TeamNeedLocation
 from main.models.action import ActionManager
 from main.models.team import Team
 from main.responses import *
 
 
+'''
 class Needs(View):
     get_dict = {
         'offset': forms.IntegerField(required=False, min_value=0),
@@ -209,3 +211,305 @@ class NeedSelf(View):
             return Http200()
         except IntegrityError:
             return Http400()
+'''
+
+
+class MemberNeed(View):
+    post_dict = {
+        'number': forms.IntegerField(min_value=1),
+        'field': forms.CharField(max_length=10),
+        'skill': forms.CharField(max_length=10),
+        'age_min': forms.IntegerField(
+            required=False, min_value=0, max_value=99),
+        'age_max': forms.IntegerField(
+            required=False, min_value=1, max_value=100),
+        'deadline': forms.DateTimeField(),
+    }
+
+    @check_object_id(Team.enabled, 'team')
+    @require_token
+    @validate_json_input(post_dict)
+    def post(self, request, team, data):
+        """
+        发布人员需求
+
+        :param team_id: 团队ID
+        :param data:
+            number: 所需人数(必填)
+            age_min: 最小年龄
+            age_max: 最大年龄
+            gender: 性别要求(默认为0,不限)
+            location: 地区要求(默认为空,不限)，格式：[province, city, county]
+            field: 领域(必填)
+            skill: 技能(必填)
+            degree: 学历(默认为0, ('其他', 0), ('初中', 1), ('高中', 2),
+                    ('大专', 3), ('本科', 4), ('硕士', 5), ('博士', 6))
+            major: 专业
+            graduate_time: 毕业时间
+            work_experience: 工作经历
+            practice_experience: 实习经历
+            project_experience: 项目经历
+            deadline: 截止时间(必填)
+        :return: need_id: 需求id
+        """
+        if request.user != team.owner:
+            return Http403('recent user has no authority')
+
+        number = data.pop('number')
+        age_min = data.pop('age_min') if 'age_min' in data else ''
+        age_max = data.pop('age_max') if 'age_max' in data else ''
+        gender = data.pop('gender') if 'gender' in data else 0
+        location = data.pop('location') if 'location' in data else None
+        field = data.pop('field')
+        skill = data.pop('skill')
+        degree = data.pop('degree') if 'degree' in data else ''
+        major = data.pop('major') if 'major' in data else ''
+        graduate_time = data.pop('graduate_time') \
+            if 'graduate_time' in data else ''
+        work_experience = data.pop('work_experience') \
+            if 'work_experience' in data else ''
+        practice_experience = data.pop('practice_experience') \
+            if 'practice_experience' in data else ''
+        project_experience = data.pop('project_experience') \
+            if 'project_experience' in data else ''
+        deadline = data.pop('deadline')
+
+        error = ''
+        try:
+            with transaction.atomic():
+                need = TeamMemberNeed(team=team, number=number, field=field,
+                                      skill=skill, deadline=deadline)
+                need.save()
+                if location:
+                    try:
+                        TeamNeedLocation.objects.set_location(need, location)
+                    except TypeError:
+                        error = 'invalid location'
+                        raise IntegrityError
+                    except ValueError as e:
+                        error = str(e)
+                        raise IntegrityError
+                if degree:
+                    need.degree = degree
+                if major:
+                    need.major = major
+                if graduate_time:
+                    need.graduate_time = graduate_time
+                if age_min and age_max:
+                    if age_min > age_max:
+                        error = 'invalid age'
+                        raise IntegrityError
+                    else:
+                        need.age_min = age_min
+                        need.age_max = age_max
+                elif age_min:
+                    need.age_min = age_min
+                elif age_max:
+                    need.age_max = age_max
+                if gender:
+                    need.gender = gender
+                if work_experience:
+                    need.work_experience = work_experience
+                if practice_experience:
+                    need.practice_experience = practice_experience
+                if project_experience:
+                    need.project_experience = practice_experience
+                need.save()
+                # 创建发布需求的动态
+                ActionManager.create_need(team, need)
+                return JsonResponse({'need_id': need.id})
+        except IntegrityError:
+            return Http400(error)
+
+
+class OutsourceNeed(View):
+    post_dict = {
+        'number': forms.IntegerField(min_value=1),
+        'field': forms.CharField(max_length=10),
+        'skill': forms.CharField(max_length=10),
+        'age_min': forms.IntegerField(
+            required=False, min_value=0, max_value=99),
+        'age_max': forms.IntegerField(
+            required=False, min_value=1, max_value=100),
+        'expend': forms.IntegerField(
+            required=False, min_value=-1),
+        'expend_unit': forms.IntegerField(
+            required=False, min_value=0, max_value=2),
+        'start_time': forms.DateTimeField(),
+        'end_time': forms.DateTimeField(),
+        'description': forms.CharField(required=False, max_length=100),
+        'deadline': forms.DateTimeField(),
+    }
+
+    @check_object_id(Team.enabled, 'team')
+    @require_token
+    @validate_json_input(post_dict)
+    def post(self, request, team, data):
+        """
+        发布外包需求
+
+        :param team_id: 团队ID
+        :param data:
+            number: 所需人数(必填)
+            age_min: 最小年龄
+            age_max: 最大年龄
+            gender: 性别要求(默认为0,不限)
+            location: 地区要求(默认为空,不限)，格式：[province, city, county]
+            field: 领域(必填)
+            skill: 技能(必填)
+            degree: 学历(默认为0, ('其他', 0), ('初中', 1), ('高中', 2),
+                    ('大专', 3), ('本科', 4), ('硕士', 5), ('博士', 6))
+            major: 专业
+            expend: 费用(默认为-1, 面谈)
+            expend_unit: 费用单位(默认为0, ('项', 0), ('天', 1), ('人', 2))
+            start_time: 外包任务开始时间(必填)
+            end_time: 外包任务结束时间(必填)
+            description: 需求描述
+            deadline: 截止时间(必填)
+        :return: need_id: 需求id
+        """
+        if request.user != team.owner:
+            return Http403('recent user has no authority')
+
+        number = data.pop('number')
+        age_min = data.pop('age_min') if 'age_min' in data else ''
+        age_max = data.pop('age_max') if 'age_max' in data else ''
+        gender = data.pop('gender') if 'gender' in data else 0
+        location = data.pop('location') if 'location' in data else None
+        field = data.pop('field')
+        skill = data.pop('skill')
+        degree = data.pop('degree') if 'degree' in data else ''
+        major = data.pop('major') if 'major' in data else ''
+        expend = data.pop('expend') if 'expend' in data else -1
+        expend_unit = data.pop('expend_unit') if 'expend_unit' in data else 0
+        start_time = data.pop('start_time')
+        end_time = data.pop('end_time')
+        description = data.pop('description') if 'description' in data else ''
+        deadline = data.pop('deadline')
+
+        if start_time > end_time:
+            return Http400('invalid time stage')
+        error = ''
+        try:
+            with transaction.atomic():
+                need = TeamOutsourceNeed(
+                    team=team, number=number, field=field, skill=skill,
+                    expend=expend, expend_unit=expend_unit,
+                    start_time=start_time, end_time=end_time, deadline=deadline)
+                need.save()
+                if location:
+                    try:
+                        TeamNeedLocation.objects.set_location(need, location)
+                    except TypeError:
+                        error = 'invalid location'
+                        raise IntegrityError
+                    except ValueError as e:
+                        error = str(e)
+                        raise IntegrityError
+                if degree:
+                    need.degree = degree
+                if major:
+                    need.major = major
+                if age_min and age_max:
+                    if age_min > age_max:
+                        error = 'invalid age'
+                        raise IntegrityError
+                    else:
+                        need.age_min = age_min
+                        need.age_max = age_max
+                elif age_min:
+                    need.age_min = age_min
+                elif age_max:
+                    need.age_max = age_max
+                if gender:
+                    need.gender = gender
+                if description:
+                    need.description = description
+                need.save()
+
+                # 创建发布需求的动态
+                ActionManager.create_need(team, need)
+                return JsonResponse({'need_id': need.id})
+        except IntegrityError:
+            return Http400(error)
+
+
+class UndertakeNeed(View):
+    post_dict = {
+        'field': forms.CharField(max_length=10),
+        'skill': forms.CharField(max_length=10),
+        'expend': forms.IntegerField(
+            required=False, min_value=-1),
+        'expend_unit': forms.IntegerField(
+            required=False, min_value=0, max_value=2),
+        'start_time': forms.DateTimeField(),
+        'end_time': forms.DateTimeField(),
+        'description': forms.CharField(required=False, max_length=100),
+        'deadline': forms.DateTimeField(),
+    }
+
+    @check_object_id(Team.enabled, 'team')
+    @require_token
+    @validate_json_input(post_dict)
+    def post(self, request, team, data):
+        """
+        发布承接需求
+
+        :param team_id: 团队ID
+        :param data:
+            location: 地区要求(默认为空,不限)，格式：[province, city, county]
+            field: 领域(必填)
+            skill: 技能(必填)
+            degree: 学历(默认为0, ('其他', 0), ('初中', 1), ('高中', 2),
+                    ('大专', 3), ('本科', 4), ('硕士', 5), ('博士', 6))
+            major: 专业
+            expend: 费用(默认为-1, 面谈)
+            expend_unit: 费用单位(默认为0, ('项', 0), ('天', 1), ('人', 2))
+            start_time: 承接开始时间(必填)
+            end_time: 承接结束时间(必填)
+            description: 需求描述
+            deadline: 截止时间(必填)
+        :return: need_id: 需求id
+        """
+        if request.user != team.owner:
+            return Http403('recent user has no authority')
+
+        location = data.pop('location') if 'location' in data else None
+        field = data.pop('field')
+        skill = data.pop('skill')
+        expend = data.pop('expend') if 'expend' in data else -1
+        expend_unit = data.pop('expend_unit') if 'expend_unit' in data else 0
+        start_time = data.pop('start_time')
+        end_time = data.pop('end_time')
+        description = data.pop('description') if 'description' in data else ''
+        deadline = data.pop('deadline')
+
+        if start_time > end_time:
+            return Http400('invalid time stage')
+        number = team.member_records.count()
+        error = ''
+        try:
+            with transaction.atomic():
+                need = TeamUndertakeNeed(
+                    team=team, number=number, field=field, skill=skill,
+                    expend=expend, expend_unit=expend_unit,
+                    start_time=start_time, end_time=end_time, deadline=deadline)
+                need.save()
+                if location:
+                    try:
+                        TeamNeedLocation.objects.set_location(need, location)
+                    except TypeError:
+                        error = 'invalid location'
+                        raise IntegrityError
+                    except ValueError as e:
+                        error = str(e)
+                        raise IntegrityError
+                if description:
+                    need.description = description
+                need.save()
+
+                # 创建发布需求的动态
+                ActionManager.create_need(team, need)
+                return JsonResponse({'need_id': need.id})
+        except IntegrityError:
+            return Http400(error)
