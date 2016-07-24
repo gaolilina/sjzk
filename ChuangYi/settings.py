a0eaaeeceefa3aec1842f5c50aa1c23a45bccd3c
DF_XYZ <dfxyz1@gmail.com>
2016-07-24 16:10:16 +0800

    clean settings

--- ChuangYi/settings.py
+++ ChuangYi/settings.py
@@ -1,93 +1,47 @@
-# ChuangYi Project Settings
-# https://docs.djangoproject.com/en/1.9/topics/settings/
-# https://docs.djangoproject.com/en/1.9/ref/settings/
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from ChuangYi import databases
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-j&$n^69qfra)^wm!asqi*+y6qef3gbcxq-efut$5g*+^1s340'
+SECRET_KEY = '$j&$n^69qfra)^wm!asqi*+y6qef3gbcxq-efut$5g*+^1s340'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 # Application definition
-
-INSTALLED_APPS = [
-    # 'django.contrib.admin',
-    # 'django.contrib.auth',
-    # 'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
-    # 'django.contrib.messages',
-    # 'django.contrib.staticfiles',
-    'main.apps.ChuangYi',
-]
-
-MIDDLEWARE_CLASSES = [
-    # 'django.middleware.security.SecurityMiddleware',
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.commons.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    # 'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'main.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'ChuangYi.wsgi.application'
+INSTALLED_APPS = ['main.apps.ChuangYi']
+ROOT_URLCONF = 'main.urls'
+MIDDLEWARE_CLASSES = []
+TEMPLATES = []
+
 
 # Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = databases.DATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db',
+    },
+}
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.9/topics/i18n/
-
 LANGUAGE_CODE = 'zh-hans'
 TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = False
 USE_L10N = False
-USE_TZ = False
+USE_TZ = True
 
 DATETIME_FORMAT = 'Y-m-d H:i:s'
+DATETIME_INPUT_FORMAT = ['%Y-%m-%d %H:%M:%S']
 DATE_FORMAT = 'Y-m-d'
-TIME_FORMAT = 'H:i:s'
+DATE_INPUT_FORMAT = ['%Y-%m-%d']
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_URL = '/upload/'
+# Media files
+MEDIA_URL = '/uploaded/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
 
 IMAGE_PATH = '%Y/%m/%d/'
