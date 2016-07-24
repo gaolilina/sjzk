7670fe2bc776b0c331cd7ff5a3268c3b867bafdb
DF_XYZ <dfxyz1@gmail.com>
2016-07-24 20:36:10 +0800

    add model User

--- main/models/__init__.py
+++ main/models/__init__.py
@@ -1,23 +1,9 @@
-# put basic models first:
-from .team import Team, TeamProfile
-from .user import User, UserToken, UserProfile, UserIdentification
+from django.db import models
 
-# then the models depending on the models above:
-from .action import UserAction, TeamAction
-from .comment import UserComment, TeamComment
-from .follow import UserFollower
-from .forum import Board, Post
-from .like import UserLiker, TeamLiker
-from .location import Province, City, UserLocation, TeamLocation
-from main.models.user.notification import Notification, NotificationReceipt
-from .tag import Tag, UserTag, TeamTag
-from .team.member import TeamMember, TeamMemberRequest, TeamInvitation
-from .team.need import TeamNeed
-from .team.task import TeamTask, TeamTaskMarker
-from .team.achievement import TeamAchievement
-from .user.experience import UserEducationExperience, UserWorkExperience, \
-    UserFieldworkExperience
-from .user.friend import UserFriend, UserFriendRequest
-from .user.message import Contact, Message
-from .team.message import UserContact, TeamContact, UserMessage, TeamMessage
-from .visitor import UserVisitor, TeamVisitor
+
+class EnabledManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_enabled=True)
+
+
+from .user import *
---
+++ main/models/user.py
@@ -0,0 +1,71 @@
+import hashlib
+from datetime import datetime
+
+from django.contrib.auth.hashers import PBKDF2PasswordHasher
+from django.db import models
+
+from ChuangYi.settings import IMAGE_PATH, PRIVATE_IMAGE_PATH
+from ..models import EnabledManager
+
+
+__all__ = ['User']
+
+
+class User(models.Model):
+    """用户模型"""
+
+    is_enabled = models.BooleanField(default=True, db_index=True)
+    username = models.CharField(max_length=20, default=None, null=True, unique=True)
+    password = models.CharField(max_length=128)
+    phone_number = models.CharField( max_length=11, unique=True)
+    token = models.CharField(max_length=32, unique=True)
+    time_created = models.DateTimeField(default=datetime.now, db_index=True)
+
+    name = models.CharField(max_length=15, db_index=True)
+    description = models.CharField(max_length=100, default='')
+    icon = models.ImageField(db_index=True, upload_to=IMAGE_PATH)
+    gender = models.CharField(max_length=1, default='')
+    qq = models.CharField(max_length=20, default='')
+    wechat = models.CharField(max_length=20, default='')
+    email = models.EmailField(default='')
+    birthday = models.DateField(default=None, null=True, db_index=True)
+    province = models.CharField(max_length=20, default='', db_index=True)
+    city = models.CharField(max_length=20, default='', db_index=True)
+    county = models.CharField(max_length=20, default='', db_index=True)
+
+    is_verified = models.BooleanField(default=False, db_index=True)
+    real_name = models.CharField(max_length=20, default='', db_index=True)
+    id_number = models.CharField(max_length=18, default='', db_index=True)
+    id_card = models.ImageField(upload_to=PRIVATE_IMAGE_PATH)
+
+    is_role_verified = models.BooleanField(default=False, db_index=True)
+    role = models.CharField(max_length=20, default='', db_index=True)
+    other_number = models.CharField(max_length=20, default='')
+    other_card = models.ImageField(upload_to=PRIVATE_IMAGE_PATH)
+    # 学校或公司
+    unit1 = models.CharField(max_length=20, default='')
+    # 学院或子部门
+    unit2 = models.CharField(max_length=20, default='')
+    # 专用或职业
+    profession = models.CharField(max_length=20, default='')
+
+    objects = models.Manager()
+    enabled = EnabledManager()
+
+    class Meta:
+        db_table = 'user'
+        ordering = ['time_created']
+
+    def set_password(self, password):
+        hasher = PBKDF2PasswordHasher()
+        self.password = hasher.encode(password, hasher.salt())
+
+    def check_password(self, password):
+        hasher = PBKDF2PasswordHasher()
+        return hasher.verify(password, self.password)
+
+    def update_token(self):
+        random_content = self.user.phone_number + datetime.now().isoformat()
+        hasher = hashlib.md5()
+        hasher.update(random_content.encode())
+        self.token = hasher.hexdigest()
