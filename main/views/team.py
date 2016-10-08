13c37db3fd18bf6a2413ab0026c909f16013c682
zq <15620953635@163.com>
2016-10-08 22:17:24 +0800

    fix bug

--- main/views/team.py
+++ main/views/team.py
@@ -216,7 +216,7 @@ class Profile(View):
         r['province'] = team.province
         r['city'] = team.city
         r['county'] = team.county
-        r['tags'] = team.tags.get_queryset().values_list('name', flat=True),
+        r['tags'] = team.tags.values_list('name', flat=True),
 
         return JsonResponse(r)
 
