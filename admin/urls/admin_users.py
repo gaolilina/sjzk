9f8937d5f2ea31818445e23d19432a55b62bcb63
Gizeta <0w0@gizeta.tk>
2016-11-20 17:28:22 +0900

    Fix admin_user

--- admin/urls/admin_users.py
+++ admin/urls/admin_users.py
@@ -4,5 +4,6 @@ from admin.views.admin_user import *
 
 urls = [
     url(r'^$', AdminUsersInfo.as_view(), name='info'),
+    url(r'^icon$', AdminUsersIcon.as_view(), name='icon'),
     url(r'^identify$', AdminUsersIndentify.as_view(), name='identify'),
 ]
--- admin/views/admin_user.py
+++ admin/views/admin_user.py
@@ -1,9 +1,10 @@
 from django import forms
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from django.template import loader, Context
 from django.views.generic import View
 
 from main.utils.decorators import validate_args
+from main.utils import identity_verify, save_uploaded_image
 
 from admin.utils.decorators import require_cookie
 
@@ -32,6 +33,25 @@ class AdminUsersInfo(View):
         context = Context({'u': user, 'msg': '保存成功'})
         return HttpResponse(template.render(context))
 
+class AdminUsersIcon(View):
+    @require_cookie
+    def post(self, request):
+        """上传用户头像"""
+
+        icon = request.FILES.get('image')
+        if not icon:
+            HttpResponseForbidden()
+
+        filename = save_uploaded_image(icon)
+        if filename:
+            request.user.icon = filename
+            request.user.save()
+
+            template = loader.get_template("admin_user/info.html")
+            context = Context({'u': request.user, 'msg': '上传成功'})
+            return HttpResponse(template.render(context))
+        HttpResponseForbidden()
+
 class AdminUsersIndentify(View):
     @require_cookie
     def get(self, request):
@@ -45,10 +65,17 @@ class AdminUsersIndentify(View):
         'id_number': forms.CharField(max_length=18, required=False),
     })
     def post(self, request, **kwargs):
+        # 调用第三方接口验证身份证的正确性
+        res = identity_verify(kwargs['id_number'])
+        error_code = res["error_code"]
+        if error_code != 0:
+            HttpResponseForbidden(res["reason"])
+        
         user = request.user
-        for k in kwargs:
-            setattr(user, k, kwargs[k])
-        user.save()
+        if not user.is_verified:
+            for k in id_keys:
+                setattr(user, k, kwargs[k])
+            user.save()
 
         template = loader.get_template("admin_user/identify.html")
         context = Context({'u': user, 'msg': '保存成功'})
