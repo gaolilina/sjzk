acf450bef1619b07ab1b5b12559f5a61366fb5ec
zhaoqiang <15620953635@163.com>
2017-01-13 14:05:14 +0800

    fix setting bug

--- ChuangYi/settings.py
+++ ChuangYi/settings.py
@@ -3,6 +3,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '$j&$n^69qfra)^wm!asqi*+y6qef3gbcxq-efut$5g*+^1s340'
