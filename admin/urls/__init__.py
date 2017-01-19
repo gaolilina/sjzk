398a057ec3e6c78e3d7b8fd1c6ff28659c508cbf
Gizeta <0w0@gizeta.tk>
2017-01-19 18:17:30 +0900

    You CANNOT register

--- admin/urls/__init__.py
+++ admin/urls/__init__.py
@@ -7,7 +7,7 @@ from admin.views.system import Setting
 
 urlpatterns = [
     url(r'^login', Login.as_view(), name='login'),
-    url(r'^register', Register.as_view(), name='register'),
+    #url(r'^register', Register.as_view(), name='register'),
     url(r'^admin_users/', include(admin_users.urls, namespace="admin_user")),
     url(r'^admin_activity/', include(admin_activity.urls, namespace="admin_activity")),
     url(r'^admin_competition/', include(admin_competition.urls, namespace="admin_competition")),
