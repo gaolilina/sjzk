c4843f3d6ffaff936124ae073e70457a4a0d68c7
Gizeta <0w0@gizeta.tk>
2017-03-23 00:20:05 +0900

    Remove generate scripts

--- generate.bat
+++
@@ -1,6 +0,0 @@
-python activity.gen.py
-python competition.gen.py
-python forum.gen.py
-python team.gen.py
-python user.gen.py
-python admin_user.gen.py
--- generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python activity.gen.py
-python competition.gen.py
-python forum.gen.py
-python team.gen.py
-python user.gen.py
-python admin_user.gen.py
