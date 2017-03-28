d038c11c4bfba63cbbe492173e96cbafcc4b6bb4
zhaoqiang <15620953635@163.com>
2017-03-28 22:37:08 +0800

    fix interface

--- main/utils/http.py
+++ main/utils/http.py
@@ -117,8 +117,9 @@ def eid_verify(data):
     conn.request('POST', '/apserver/login', data, headers)
     response = conn.getresponse()
     content = response.read().decode('utf-8')
-    res = json.loads(content)
-    if res['result'] == "00":
+    print (content)
+    # res = json.loads(content)
+    if content['result'] == "00":
         return 1
     else:
         return 0
