1d48507fc8b2df1101845bf7e43a4d0de67e3ea9
Gizeta <i@gizeta.me>
2018-06-22 15:53:06 +0800

    fix missing import

--- main/views/vote.py
+++ main/views/vote.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.utils import timezone
 from django.views.generic import View
 
-from ..models import UserVote, UserVoteOption, UserVoteOptionAdvocator, LabVote, LabVoteOption, LabVoteOptionAdvocator
+from ..models import UserVote, UserVoteOption, UserVoteOptionAdvocator, LabVote, LabVoteOption, LabVoteOptionAdvocator, User, Lab
 from ..utils import abort
 from ..utils.decorators import *
 
