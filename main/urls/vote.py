b9b75fb8afac8b4ae83f98bb82b04b05a10117a4
Gizeta <i@gizeta.me>
2018-06-22 15:55:24 +0800

    fix wrong view name

--- main/urls/vote.py
+++ main/urls/vote.py
@@ -7,7 +7,7 @@ urls = [
     url(r'^user/(?P<user_id>[0-9]+)/$', UserList.as_view(), name='user_list'),
     url(r'^user/(?P<vote_id>[0-9]+)/$', UserDetail.as_view(), name='user_detail'),
     url(r'^user/(?P<option_id>[0-9]+)/$', UserOption.as_view(), name='user_option'),
-    url(r'^lab/(?P<lab_id>[0-9]+)/$', TeamList.as_view(), name='lab_list'),
-    url(r'^lab/(?P<vote_id>[0-9]+)/$', TeamDetail.as_view(), name='lab_detail'),
-    url(r'^lab/(?P<option_id>[0-9]+)/$', TeamOption.as_view(), name='lab_option'),
+    url(r'^lab/(?P<lab_id>[0-9]+)/$', LabList.as_view(), name='lab_list'),
+    url(r'^lab/(?P<vote_id>[0-9]+)/$', LabDetail.as_view(), name='lab_detail'),
+    url(r'^lab/(?P<option_id>[0-9]+)/$', LabOption.as_view(), name='lab_option'),
 ]
