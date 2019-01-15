8b9f5d110f213d49a49aea0921a37f8a2c815af1
HowieWolf <howiewh@163.com>
2019-01-16 00:23:04 +0800

    fix

--- main/models/achievement.py
+++ main/models/achievement.py
@@ -13,7 +13,6 @@ class UserAchievement(models.Model):
     description = models.CharField(max_length=100, default='')
     picture = models.CharField(max_length=255, default='')
     time_created = models.DateTimeField(default=timezone.now, db_index=True)
-    likers = models.ManyToManyField('User', through='UserAchievementLiker')
     requirers = models.ManyToManyField('User', related_name='requireAchievements')
 
     class Meta:
