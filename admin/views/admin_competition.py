423dc1e9923d05f72025c72d3dc9c1338cabf7fd
HowieWolf <howiewh@163.com>
2019-05-07 23:32:07 +0800

    fix

--- admin/views/admin_competition.py
+++ admin/views/admin_competition.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django import forms
 from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
 from django.template import loader, Context
@@ -13,6 +15,7 @@ from admin.models.admin_user import AdminUser
 
 from admin.utils.decorators import *
 from main.utils import *
+
 
 class AdminCompetitionAdd(View):
     @require_cookie
@@ -44,34 +47,39 @@ class AdminCompetitionAdd(View):
     def post(self, request, **kwargs):
         user = request.user
         competition = Competition()
+        dateformat = "%Y-%m-%d"
 
         if 'stages' in kwargs and kwargs['stages'] != "":
             stages = json.loads(kwargs['stages'])
             for st in stages:
-                if st['time_started'] < kwargs['time_started'] or st['time_ended'] > kwargs['time_ended']:
+                if datetime.strptime(st['time_started'], dateformat) < kwargs['time_started'] or datetime.strptime(
+                        st['time_ended'], dateformat) > kwargs['time_ended']:
                     return HttpResponseForbidden('时间输入有误')
 
         for k in kwargs:
             if k == 'owner':
-                CompetitionOwner.objects.create(competition=competition, user=AdminUser.objects.filter(pk=kwargs['owner']).get())
+                CompetitionOwner.objects.create(competition=competition,
+                                                user=AdminUser.objects.filter(pk=kwargs['owner']).get())
             elif k != "stages":
                 setattr(competition, k, kwargs[k])
         competition.save()
 
         stages = json.loads(kwargs['stages'])
         for st in stages:
-            competition.stages.create(status=int(st['status']), time_started=st['time_started'], time_ended=st['time_ended'])
+            competition.stages.create(status=int(st['status']), time_started=st['time_started'],
+                                      time_ended=st['time_ended'])
 
         template = loader.get_template("admin_competition/add.html")
         context = Context({'msg': '保存成功', 'user': request.user})
         return HttpResponse(template.render(context))
+
 
 class AdminCompetitionEdit(View):
     @fetch_record(Competition.enabled, 'model', 'id')
     @require_cookie
     @require_role('xyz')
     def get(self, request, model):
-        #if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
+        # if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
         #    return HttpResponseForbidden()
 
         template = loader.get_template("admin_competition/edit.html")
@@ -108,40 +116,46 @@ class AdminCompetitionEdit(View):
     def post(self, request, **kwargs):
         user = request.user
         model = kwargs["model"]
+        dateformat = "%Y-%m-%d"
 
         if 'stages' in kwargs and kwargs['stages'] != "":
             stages = json.loads(kwargs['stages'])
             for st in stages:
-                if st['time_start'] < kwargs['time_started'] or st['time_ended'] > kwargs['time_ended']:
+                if datetime.strptime(st['time_started'], dateformat) < kwargs['time_started'] or datetime.strptime(
+                        st['time_ended'], dateformat) > kwargs['time_ended']:
                     return HttpResponseForbidden('时间输入有误')
 
-        #if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
+        # if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
         #    return HttpResponseForbidden()
 
         for k in kwargs:
             if k == 'owner':
-                CompetitionOwner.objects.filter(competition=model).update(user=AdminUser.objects.filter(pk=kwargs['owner']).get())
+                CompetitionOwner.objects.filter(competition=model).update(
+                    user=AdminUser.objects.filter(pk=kwargs['owner']).get())
             elif k != "stages":
                 setattr(model, k, kwargs[k])
         model.save()
-        
+
         if 'stages' in kwargs and kwargs['stages'] != "":
             CompetitionStage.objects.filter(competition=model).delete()
 
             stages = json.loads(kwargs['stages'])
             for st in stages:
-                model.stages.create(status=int(st['status']), time_started=st['time_started'], time_ended=st['time_ended'])
+                model.stages.create(status=int(st['status']), time_started=st['time_started'],
+                                    time_ended=st['time_ended'])
 
         template = loader.get_template("admin_competition/edit.html")
-        context = Context({'model': model, 'msg': '保存成功', 'user': request.user, 'stages': CompetitionStage.objects.filter(competition=model)})
+        context = Context({'model': model, 'msg': '保存成功', 'user': request.user,
+                           'stages': CompetitionStage.objects.filter(competition=model)})
         return HttpResponse(template.render(context))
+
 
 class AdminCompetitionList(View):
     @require_cookie
     @require_role('axyz')
     def get(self, request):
         try:
-            filter_param = {} # TODO: same with activity
+            filter_param = {}  # TODO: same with activity
             if not request.user.can_x():
                 filter_param['user'] = request.user
             template = loader.get_template("admin_competition/list.html")
@@ -152,60 +166,66 @@ class AdminCompetitionList(View):
             context = Context({'user': request.user})
             return HttpResponse(template.render(context))
 
+
 class AdminCompetitionView(View):
     @fetch_record(Competition.enabled, 'model', 'id')
     @require_cookie
     @require_role('axyz')
     def get(self, request, model):
-        #if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
+        # if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
         #    return HttpResponseForbidden()
-            
+
         template = loader.get_template("admin_competition/view.html")
-        context = Context({'model': model, 'user': request.user, 'stages': CompetitionStage.objects.filter(competition=model)})
+        context = Context(
+            {'model': model, 'user': request.user, 'stages': CompetitionStage.objects.filter(competition=model)})
         return HttpResponse(template.render(context))
+
 
 class AdminCompetitionFilesView(View):
     @fetch_record(Competition.enabled, 'model', 'id')
     @require_cookie
     @require_role('axyz')
     def get(self, request, model, status):
-        #if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
+        # if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
         #    return HttpResponseForbidden()
 
         template = loader.get_template("admin_competition/file.html")
         context = Context({'model': model, 'user': request.user,
-            'files': [{
-                'team': file.team,
-                'file': file.file,
-                'id': file.id,
-                'time_created': file.time_created,
-                'participator': CompetitionTeamParticipator.objects.filter(competition=model, team=file.team).get(),
-                'type': file.type,
-                'score': file.score,
-                'comment': file.comment,
-            } for file in CompetitionFile.objects.filter(competition=model, status=status)],
-            'teams': CompetitionTeamParticipator.objects.filter(competition=model, final=True).all()})
+                           'files': [{
+                               'team': file.team,
+                               'file': file.file,
+                               'id': file.id,
+                               'time_created': file.time_created,
+                               'participator': CompetitionTeamParticipator.objects.filter(competition=model,
+                                                                                          team=file.team).get(),
+                               'type': file.type,
+                               'score': file.score,
+                               'comment': file.comment,
+                           } for file in CompetitionFile.objects.filter(competition=model, status=status)],
+                           'teams': CompetitionTeamParticipator.objects.filter(competition=model, final=True).all()})
         return HttpResponse(template.render(context))
+
 
 class AdminCompetitionExcelView(View):
     @fetch_record(Competition.enabled, 'model', 'id')
     @require_cookie
     @require_role('axyz')
     def get(self, request, model):
-        #if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
+        # if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
         #    return HttpResponseForbidden()
 
         template = loader.get_template("admin_competition/excel.html")
         context = Context({'model': model})
         return HttpResponse(template.render(context),
-            content_type="text/csv")
+                            content_type="text/csv")
+
 
 class AdminCompetitionAwardEdit(View):
     @fetch_record(Competition.enabled, 'model', 'id')
     @require_cookie
     @require_role('axyz')
     def get(self, request, model):
-        #if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
+        # if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
         #    return HttpResponseForbidden()
 
         template = loader.get_template("admin_competition/award.html")
@@ -222,9 +242,9 @@ class AdminCompetitionAwardEdit(View):
         user = request.user
         model = kwargs["model"]
 
-        #if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
+        # if len(CompetitionOwner.objects.filter(competition=model, user=request.user)) == 0:
         #    return HttpResponseForbidden()
-        
+
         awards = json.loads(kwargs['awards'])
         for a in awards:
             for id in awards[a]:
@@ -250,6 +270,7 @@ class CompetitionFileExpert(View):
         ).update(rater=expert)
         abort(200)
 
+
 class CompetitionExpertList(View):
     @fetch_object(Competition.enabled, 'competition')
     @require_cookie
@@ -261,33 +282,33 @@ class CompetitionExpertList(View):
             c = competition.experts.all().count()
             qs = competition.experts.all()
             l = [{'id': user.id,
-                'time_created': user.time_created,
-                'name': user.name,
-                'icon_url': user.icon,
-                'description': user.description,
-                'email': user.email,
-                'gender': user.gender,
-                'birthday': user.birthday,
-                'province': user.province,
-                'city': user.city,
-                'county': user.county,
-                'follower_count': user.followers.count(),
-                'followed_count': user.followed_users.count() + user.followed_teams.count(),
-                'friend_count': user.friends.count(),
-                'liker_count': user.likers.count(),
-                'visitor_count': user.visitors.count(),
-                'is_verified': user.is_verified,
-                'is_role_verified': user.is_role_verified,
-                'role': user.role,
-                'adept_field': user.adept_field,
-                'adept_skill': user.adept_skill,
-                'expect_role': user.expect_role,
-                'follow_field': user.follow_field,
-                'follow_skill': user.follow_skill,
-                'unit1': user.unit1,
-                'unit2': user.unit2,
-                'profession': user.profession,
-                'score': user.score} for user in qs]
+                  'time_created': user.time_created,
+                  'name': user.name,
+                  'icon_url': user.icon,
+                  'description': user.description,
+                  'email': user.email,
+                  'gender': user.gender,
+                  'birthday': user.birthday,
+                  'province': user.province,
+                  'city': user.city,
+                  'county': user.county,
+                  'follower_count': user.followers.count(),
+                  'followed_count': user.followed_users.count() + user.followed_teams.count(),
+                  'friend_count': user.friends.count(),
+                  'liker_count': user.likers.count(),
+                  'visitor_count': user.visitors.count(),
+                  'is_verified': user.is_verified,
+                  'is_role_verified': user.is_role_verified,
+                  'role': user.role,
+                  'adept_field': user.adept_field,
+                  'adept_skill': user.adept_skill,
+                  'expect_role': user.expect_role,
+                  'follow_field': user.follow_field,
+                  'follow_skill': user.follow_skill,
+                  'unit1': user.unit1,
+                  'unit2': user.unit2,
+                  'profession': user.profession,
+                  'score': user.score} for user in qs]
             return JsonResponse({'count': c, 'list': l})
         template = loader.get_template("admin_competition/add_expert.html")
         context = Context({
@@ -298,7 +319,7 @@ class CompetitionExpertList(View):
             'participators': competition.team_participators.all(),
         })
         return HttpResponse(template.render(context))
-    
+
     @fetch_object(Competition.enabled, 'competition')
     @require_cookie
     @validate_args({
@@ -308,6 +329,7 @@ class CompetitionExpertList(View):
         expert = User.objects.filter(pk=expert_id).get()
         competition.experts.add(expert)
         abort(200)
+
 
 class CompetitionTeamFinal(View):
     @fetch_object(Competition.enabled, 'competition')
@@ -320,6 +342,7 @@ class CompetitionTeamFinal(View):
         for t in teams:
             CompetitionTeamParticipator.objects.filter(competition=competition, team_id=int(t)).update(final=False)
         abort(200)
+
 
 class CompetitionTeamList(View):
     @fetch_object(Competition.enabled, 'competition')
