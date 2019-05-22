76487d5179f753489b405b98e1883b3e11aa7383
bwlu <18232386357@163.com>
2019-05-22 10:49:42 +0800

    namespace='security_log'

--- admin/urls/__init__.py
+++ admin/urls/__init__.py
@@ -24,5 +24,6 @@ urlpatterns = [
     url(r'^system_notification/', Notification.as_view(), name='notification'),
     url(r'^admin_user/', include(admin_user.urls, namespace='admin_user')),
     url(r'^op_log/', OpLog.as_view(), name='op_log'),
-    url(r'^security_log/', include(security.urls), name='se_log'),
+    # url(r'^security_log/', include(security.urls), name='se_log'),
+    url(r'^security_log/', include(security.urls, namespace='security_log')),
 ]
