7079c8bb5f6f8cf1778580fba8987c7feb3cd05c
bwlu <18232386357@163.com>
2019-07-20 16:21:23 +0800

    测试

--- web/views/activity.py
+++ web/views/activity.py
@@ -40,6 +40,8 @@ class AdminActivityAdd(View):
         activity.save()
 
         stages = json.loads(kwargs['stages'])
+        print(kwargs['stages'])
+        print(stages)
         for st in stages:
             activity.stages.create(status=int(st['status']), time_started=st['time_started'], time_ended=st['time_ended'])
         return JsonResponse({
