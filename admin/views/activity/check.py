ff4ba3d3910288c81e5fa9b6dfdd937719d26f41
HowieWang <howiewh@163.com>
2019-07-27 18:00:07 +0800

    fix activity bug

--- admin/views/activity/check.py
+++ admin/views/activity/check.py
@@ -1,29 +1,29 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 from django import forms
-from django.http import JsonResponse
-from django.views.generic.base import View
 
 from admin.utils.decorators import require_role
-from util.decorator.auth import admin_auth
 from main.models import Activity
-from main.utils import abort
+from util.base.view import BaseView
+from util.decorator.auth import cms_auth
 from util.decorator.param import fetch_object, validate_args
 
 
-class ActivityCheck(View):
+class ActivityCheck(BaseView):
 
-    @admin_auth
+    @cms_auth
     @require_role('xyz')
     @validate_args({
         'activity_id': forms.IntegerField(),
-        'result': forms.BooleanField(required=False),
+        'result': forms.BooleanField(),
     })
     @fetch_object(Activity.objects, 'activity')
     def post(self, request, activity, result, **kwargs):
+        if activity.state == Activity.STATE_NO:
+            return self.fail(1, '活动已被拒绝，需要用户重新提交材料')
+        if activity.state == Activity.STATE_PASSED:
+            return self.success()
         if activity.state == Activity.STATE_CHECKING:
             Activity.objects.filter(id=activity.id) \
                 .update(state=Activity.STATE_PASSED if result else Activity.STATE_NO)
-            return JsonResponse({})
-        else:
-            abort(403, "重新提交材料")
+            return self.success()
