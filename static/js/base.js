595d58f97e19902767ee16d9b8ade8d5070ebc0a
bwlu <18232386357@163.com>
2019-08-06 14:20:15 +0800

    活动统计

--- static/js/base.js
+++ static/js/base.js
@@ -17,6 +17,16 @@ function getRequest() {
   }
   return theRequest; 
 }
+//token
+function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+
+    if (arr = document.cookie.match(reg))
+
+        return unescape(arr[2]);
+    else
+        return null;
+}
 
 /*jqueryUI
 */
