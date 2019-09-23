41139875ea4a88894326219d12f8ce58747ab540
HowieWang <howiewh@163.com>
2019-09-23 20:08:33 +0800

    添加 phone 字段

--- main/views/user.py
+++ main/views/user.py
@@ -87,6 +87,7 @@ class Profile(View):
         r = {'id': user.id,
              'time_created': user.time_created,
              'name': user.name,
+             'phone': user.phone_number,
              'icon_url': user.icon,
              'description': user.description,
              'email': user.email,
