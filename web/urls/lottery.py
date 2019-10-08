d9830aea86ad0fcc94ffe101f88af1888d092dbc
HowieWang <howiewh@163.com>
2019-10-08 23:26:09 +0800

    扫码抽奖

--- web/urls/lottery.py
+++ web/urls/lottery.py
@@ -3,7 +3,7 @@
 from django.conf.urls import url
 
 from web.views.lottery.action import JoinLottery, LotteryAction
-from web.views.lottery.award import ReceiveAward
+from web.views.lottery.award import ReceiveAward, UserReceiveAward
 from web.views.lottery.list_detail import LotteryListAndCreate, LotteryInfo
 from web.views.lottery.user import LotteryJoinedUserList, MyVictoryList
 
@@ -18,7 +18,9 @@ urlpatterns = [
     url(r'^(?P<lottery_id>\d+)/victory/$', LotteryAction.as_view()),
     # 抽奖池中的用户
     url(r'^(?P<lottery_id>\d+)/users/$', LotteryJoinedUserList.as_view()),
-    # 领取奖品
+    # 领取奖品，用户主动领取奖品
+    url(r'^(?P<lottery_id>\d+)/receive/$', UserReceiveAward.as_view()),
+    # 领取奖品，抽奖方标识奖品被领走
     url(r'award/(?P<award_id>\d+)/receive/$', ReceiveAward.as_view()),
     # 我的中奖信息
     url(r'^my/victory/$', MyVictoryList.as_view()),
--- web/views/lottery/award.py
+++ web/views/lottery/award.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from modellib.models import LotteryParticipant
+from modellib.models import LotteryParticipant, Lottery
 from util.base.view import BaseView
 from util.decorator.auth import client_auth
 from util.decorator.param import validate_args, fetch_object
@@ -17,4 +17,19 @@ class ReceiveAward(BaseView):
         if award.lottery.user != request.user:
             return self.fail(1, '无权操作')
         LotteryParticipant.objects.filter(id=award.id).update(is_handled=True)
+        return self.success()
+
+
+class UserReceiveAward(BaseView):
+
+    @client_auth
+    @validate_args({
+        'lottery_id': forms.IntegerField(),
+    })
+    @fetch_object(Lottery.objects, 'lottery')
+    def post(self, request, lottery, **kwargs):
+        qs = LotteryParticipant.objects.filter(lottery=lottery, user=request.user)
+        if not qs.exists():
+            return self.fail(1, '无中奖信息')
+        LotteryParticipant.objects.filter(id=qs.first().id).update(is_handled=True)
         return self.success()
