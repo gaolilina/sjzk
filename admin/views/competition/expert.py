c0f2d9df1820c6be69bff39bae64c141f7f1aa13
bwlu <18232386357@163.com>
2019-10-09 11:33:45 +0800

    remove assign expert file

--- admin/views/competition/expert.py
+++ admin/views/competition/expert.py
@@ -77,24 +77,12 @@ class TeamExpert(BaseView):
     @admin_auth
     @validate_args({
         'competition_id': forms.IntegerField(),
-        'status': forms.IntegerField(),
     })
     @fetch_object(Competition.enabled, 'competition')
-    def get(self, request, competition, status, **kwargs):
+    def get(self, request, competition, **kwargs):
         template = loader.get_template("admin_competition/file.html")
         context = Context({
             'model': competition, 'user': request.user,
-            'files': [{
-                'team': file.team,
-                'file': file.file,
-                'id': file.id,
-                'time_created': file.time_created,
-                'participator': CompetitionTeamParticipator.objects.filter(competition=competition,
-                                                                           team=file.team).get(),
-                'type': file.type,
-                'score': file.score,
-                'comment': file.comment,
-            } for file in CompetitionFile.objects.filter(competition=competition, status=status)],
             'teams': CompetitionTeamParticipator.objects.filter(competition=competition, final=False).all()
         })
         return HttpResponse(template.render(context))
