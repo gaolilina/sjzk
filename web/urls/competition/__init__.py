e267cb8623d429ce1e49969a12bce6ecc6f5c20e
HowieWang <howiewh@163.com>
2019-10-15 23:24:08 +0800

    我评分的竞赛

--- web/urls/competition/__init__.py
+++ web/urls/competition/__init__.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
 from web.views.competition.file import TeamFile, ExpertRateFile
-from web.views.competition.list import MyJoinedCompetition
+from web.views.competition.list import MyJoinedCompetition, MyRatingCompetition
 from web.views.competition.score import FileScore
 
 urlpatterns = [
     url(r'^joined/$', MyJoinedCompetition.as_view()),
+    url(r'^rating/$', MyRatingCompetition.as_view()),
     url(r'^(?P<competition_id>\d+)/ratefile/$', ExpertRateFile.as_view()),
     url(r'^(?P<competition_id>\d+)/file/team/(?P<team_id>\d+)/$', TeamFile.as_view()),
 
--- web/views/competition/list.py
+++ web/views/competition/list.py
@@ -34,16 +34,44 @@ class MyJoinedCompetition(BaseView):
 
         qs = CompetitionTeamParticipator.objects.filter(**condition)
         c = qs.count()
-        l = [{
-            'id': a.competition.id,
-            'name': a.competition.name,
-            'liker_count': a.competition.likers.count(),
-            'time_started': a.competition.time_started,
-            'time_ended': a.competition.time_ended,
-            'team_participator_count': a.competition.team_participators.count(),
-            'time_created': a.competition.time_created,
-            'status': a.competition.status,
-            'field': a.competition.field,
-            'province': a.competition.province,
-        } for a in qs[page * limit:(page + 1) * limit]]
+        l = [competition_to_json(a.competition) for a in qs[page * limit:(page + 1) * limit]]
         return self.success({'count': c, 'list': l})
+
+
+class MyRatingCompetition(BaseView):
+
+    @client_auth
+    @validate_args({
+        'page': forms.IntegerField(required=False, min_value=0),
+        'limit': forms.IntegerField(required=False, min_value=0),
+    })
+    def get(self, request, page=0, limit=CONSTANT_DEFAULT_LIMIT, **kwargs):
+        """
+        我评分的竞赛
+        """
+        # 只显示未结束的活动
+        condition = {
+            'rater': request.user,
+            'competition__time_ended__gt': datetime.datetime.now(),
+            'competition__status__in': [0, 1, 2, 3, 4, 5],
+            'final': False,
+        }
+        qs = CompetitionTeamParticipator.objects.filter(**condition)
+        c = qs.count()
+        l = [competition_to_json(a.competition) for a in qs[page * limit:(page + 1) * limit]]
+        return self.success({'count': c, 'list': l})
+
+
+def competition_to_json(competition):
+    return {
+        'id': competition.id,
+        'name': competition.name,
+        'liker_count': competition.likers.count(),
+        'time_started': competition.time_started,
+        'time_ended': competition.time_ended,
+        'team_participator_count': competition.team_participators.count(),
+        'time_created': competition.time_created,
+        'status': competition.status,
+        'field': competition.field,
+        'province': competition.province,
+    }
