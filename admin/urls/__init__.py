6629ae31f14ec1343cbe3bf0bc44eba699ed940e
howiewang <howiewh@163.com>
2020-05-07 01:04:51 +0800

    admin 完善注释

--- admin/urls/__init__.py
+++ admin/urls/__init__.py
@@ -11,23 +11,39 @@ from admin.views.op_log import OpLog
 urlpatterns = [
     url(r'^$', Main.as_view(), name='root'),
     url(r'^login', Login.as_view(), name='login'),
-    url(r'^register', Register.as_view(), name='register'),
-    url(r'^admin_users/', include(admin_users.urls, namespace="admin_users")),
+
+    # 系统配置
+    url(r'^system_admin/', Setting.as_view(), name='system'),
+
+    # 系统通知
+    url(r'^system_notification/', Notification.as_view(), name='notification'),
+
+    # 调查问卷
+    url(r'^paper/', include(paper.urls, namespace='paper')),
+
+    # 日志
+    url(r'^op_log/', OpLog.as_view(), name='op_log'), # 管理端操作日志
+    url(r'^security_log/', include(security.urls, namespace='security_log')), # 客户端安全日志
+
+    # 竞赛的业务流程
+    url(r'^admin_competition/', include(admin_competition.urls, namespace="admin_competition")),
+
+    # 活动的业务流程
+    url(r'^activity/', include(activity.urls, namespace="activity")),
+
+    # 纯粹的客户端数据管理
+    url(r'^competition_admin/', include(competition, namespace="competition")), # 竞赛对象的数据管理
+    url(r'^admin_activity/', include(client.urls, namespace="admin_activity")), # 活动对象的数据管理
     url(r'^feedback/', include(feedback.urls, namespace="feedback")),
     url(r'^user_admin/', include(user.urls, namespace="user")),
     url(r'^team_admin/', include(team.urls, namespace="team")),
     url(r'^forum_admin/', include(forum.urls, namespace="forum")),
-    url(r'^system_admin/', Setting.as_view(), name='system'),
-    url(r'^system_notification/', Notification.as_view(), name='notification'),
-    url(r'^admin_user/', include(admin_user.urls, namespace='admin_user')),
-    url(r'^op_log/', OpLog.as_view(), name='op_log'),
-    url(r'^paper/', include(paper.urls, namespace='paper')),
-    url(r'^security_log/', include(security.urls, namespace='security_log')),
 
-    url(r'^competition_admin/', include(competition, namespace="competition")),
-    url(r'^admin_competition/', include(admin_competition.urls, namespace="admin_competition")),
+    # 以下的弃用，已迁移至 cms
+    # 管理员
+    url(r'^admin_users/', include(admin_users.urls, namespace="admin_users")), # 管理自己的信息
+    url(r'^admin_user/', include(admin_user.urls, namespace='admin_user')), # 作为管理员管理其他管理员
 
-    # 活动，以后仅在 activity 这个 url，另外两个不在维护
-    url(r'^admin_activity/', include(client.urls, namespace="admin_activity")),
-    url(r'^activity/', include(activity.urls, namespace="activity")),
+    # 注册管理员
+    url(r'^register', Register.as_view(), name='register'),
 ]
--- admin/urls/paper.py
+++ admin/urls/paper.py
@@ -7,9 +7,9 @@ from admin.views.analysis import PaperAnalysis
 from admin.views.paper import PaperList, PaperDetail, PaperSwitch, PaperAdd
 
 urls = [
-    url(r'^$', PaperList.as_view(), name='list'),
-    url(r'^add/$', PaperAdd.as_view(), name='add'),
-    url(r'^(?P<paper_id>\d+)/$', PaperDetail.as_view(), name='detail'),
-    url(r'^(?P<paper_id>\d+)/switch/$', PaperSwitch.as_view(), name='switch'),
-    url(r'^(?P<paper_id>\d+)/analysis/$', PaperAnalysis.as_view(), name='analysis'),
+    url(r'^$', PaperList.as_view(), name='list'), # 调查问卷列表
+    url(r'^add/$', PaperAdd.as_view(), name='add'), # 创建调查问卷
+    url(r'^(?P<paper_id>\d+)/$', PaperDetail.as_view(), name='detail'), # 调查问卷详情
+    url(r'^(?P<paper_id>\d+)/switch/$', PaperSwitch.as_view(), name='switch'), # 开启/关闭调查问卷
+    url(r'^(?P<paper_id>\d+)/analysis/$', PaperAnalysis.as_view(), name='analysis'), # 分析调查问卷
 ]
